28ded6c1f8d1e8a471dff9c5e2561bc8f53b8d03
LisonFan <lisonfan1996@gmail.com>
2017-02-10 09:30:02 +0800

    Frist Push

---
+++ zimuzuplus.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name			字幕组字幕下载页面扩展脚本
+// @namespace		https://lisonfan.com/
+// @icon			https://lisonfan.com/favicon.ico
+// @author			LisonFan
+// @version			0.5
+// @description		给字幕组的字幕下载页面增加一个字幕版本对应的视频文件下载链接，这样描述应该看得懂吧。。。。
+// @match			http://www.zmz2017.com/subtitle/*
+// @grant			none
+// @license			MIT License
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var text = document.getElementsByClassName('subtitle-info')[0].innerText;
+    var textGSIndex = text.search("【格式】");
+    var textBBIndex = text.search("【版本】");
+    var text2 = text.substring(textGSIndex,textBBIndex);
+    var text3 = text2.substring(4,textGSIndex);
+	var arr = text3.split(" ");
+	for(var i = 0; i < arr.length; i++){
+	    var urlStr = 'https://rarbg.to/torrents.php?search=' + arr[i];
+	    var url = "<h3>" + "字幕对应视频下载链接：" + "<a href='" + urlStr + "'target='_blank'>" + arr[i] + "</a>" + "</h3>";
+	    var test4 = document.getElementsByClassName('subtitle-links tc')[0].innerHTML;
+	    document.getElementsByClassName('subtitle-links tc')[0].innerHTML = test4 + url;
+	}
+})();
